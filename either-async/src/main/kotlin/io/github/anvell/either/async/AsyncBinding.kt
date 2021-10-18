0e0155f4540dde1a33673debdece54a58b303897
Denis Trotsenko <denis.tnko@gmail.com>
2021-10-18 20:34:03 +0200

    Allow to start eitherAsync lazily

--- either-async/src/main/kotlin/io/github/anvell/either/async/AsyncBinding.kt
+++ either-async/src/main/kotlin/io/github/anvell/either/async/AsyncBinding.kt
@@ -6,15 +6,20 @@ import io.github.anvell.either.Either
 import io.github.anvell.either.Left
 import io.github.anvell.either.Right
 import io.github.anvell.either.fold
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.async
+import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
+/**
+ * Allows to compose a set of [Either] values in an imperative way
+ * using suspendable [bind][EitherCoroutineScope.bind] function.
+ *
+ * Result is returned as [Deferred] object.
+ */
 fun <L : Any, R> CoroutineScope.eitherAsync(
+    start: CoroutineStart = CoroutineStart.DEFAULT,
     block: suspend EitherCoroutineScope<L>.() -> R
 ): Deferred<Either<L, R>> {
-    return async {
+    return async(start = start) {
         with(EitherCoroutineScopeImpl<L>(coroutineContext)) {
             try {
                 Right(block())
