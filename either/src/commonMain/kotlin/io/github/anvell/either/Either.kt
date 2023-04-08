545e59d941a328048b56dc3cc24d38e3583801fc
Denis Trotsenko <denis.tnko@gmail.com>
2023-04-08 22:47:40 +0200

    Extract `eitherCatch` to separate file

--- either/src/commonMain/kotlin/io/github/anvell/either/Either.kt
+++ either/src/commonMain/kotlin/io/github/anvell/either/Either.kt
@@ -2,8 +2,6 @@
 
 package io.github.anvell.either
 
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.TimeoutCancellationException
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
@@ -47,29 +45,6 @@ public class Right<out R>(public val value: R) : Either<Nothing, R>() {
         if (other == null || this::class != other::class) return false
         other as Right<*>
         return value == other.value
-    }
-}
-
-/**
- * Calls the specified function [block] and returns its encapsulated result as [Right]
- * if invocation was successful, catching any [Throwable] exception that was
- * thrown from the [block] function execution and encapsulating it as [Left].
- *
- * In order to avoid breaking structured concurrency of coroutines [CancellationException]
- * is re-thrown.
- */
-public inline fun <R> eitherCatch(block: () -> R): Either<Throwable, R> {
-    contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-    }
-    return try {
-        Right(block())
-    } catch (e: Throwable) {
-        when (e) {
-            is TimeoutCancellationException -> Left(e)
-            is CancellationException -> throw e
-            else -> Left(e)
-        }
     }
 }
 
