a79af04f14fad4004030e27e51cbf8e343e3ce50
Denis Trotsenko <denis.tnko@gmail.com>
2023-04-10 23:52:59 +0200

    Add raise for left values

--- either/src/commonMain/kotlin/io/github/anvell/either/binding/EitherScope.kt
+++ either/src/commonMain/kotlin/io/github/anvell/either/binding/EitherScope.kt
@@ -4,4 +4,5 @@ import io.github.anvell.either.Either
 
 public interface EitherScope<L : Any> {
     public fun <R> Either<L, R>.bind(): R
+    public fun raise(value: L): Nothing
 }
--- either/src/commonMain/kotlin/io/github/anvell/either/binding/internal/EitherScopeImpl.kt
+++ either/src/commonMain/kotlin/io/github/anvell/either/binding/internal/EitherScopeImpl.kt
@@ -10,12 +10,14 @@ internal class EitherScopeImpl<L : Any> : EitherScope<L> {
     lateinit var left: L
 
     override fun <R> Either<L, R>.bind(): R = when (this) {
-        is Left -> {
-            if (::left.isInitialized.not()) {
-                left = value
-            }
-            throw BindingCancellationException()
-        }
+        is Left -> raise(value)
         is Right -> value
+    }
+
+    override fun raise(value: L): Nothing {
+        if (::left.isInitialized.not()) {
+            left = value
+        }
+        throw BindingCancellationException()
     }
 }
